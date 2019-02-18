01265b3170480e1f731d56339ba993d417896fb0
ghttt <ghting8612@gmail.com>
2019-02-18 15:19:52 +0800

    Update BMPlayerControlView.swift
    点击button pause状态时controlview不消失

--- Source/BMPlayerControlView.swift
+++ Source/BMPlayerControlView.swift
@@ -379,14 +379,18 @@ open class BMPlayerControlView: UIView {
      - parameter button: action Button
      */
     @objc open func onButtonPressed(_ button: UIButton) {
-        autoFadeOutControlViewWithAnimation()
+        
         if let type = ButtonType(rawValue: button.tag) {
             switch type {
             case .play, .replay:
+                if player!.isPlaying {
+                   autoFadeOutControlViewWithAnimation()
+                }
                 if playerLastState == .playedToTheEnd {
                     hidePlayToTheEndView()
                 }
             default:
+                  autoFadeOutControlViewWithAnimation()
                 break
             }
         }
